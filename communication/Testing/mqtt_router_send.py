60a5dc44ea54e1d59f17eaeeeb4cde78b2d2347a
Uzair Ahmed <u1ahmed@ryerson.ca>
2023-01-26 23:37:38 -0500

    push

--- communication/Testing/mqtt_router_send.py
+++ communication/Testing/mqtt_router_send.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as mqtt
-
+#opkg install python3-pip
 mqttc = mqtt.Client("digi_mqtt_test")  # Create instance of client with client ID “digitest”
 mqttc.connect("192.168.1.116", 1883)  # Connect to (broker, port, keepalive-time)
 mqttc.loop_start()  # Start networking daemon
-for i in range(0,1):
+for i in range(0,150):
     mqttc.publish("router/logs", "connected-device")  # Publish message to “digitest /test1” topic
+    mqttc.publish("iOT_1/logs", "connected-device")  # Publish message to “digitest /test1” topic
+    mqttc.publish("iOT_2/logs", "connected-device")  # Publish message to “digitest /test1” topic
+    mqttc.publish("iOT_3/logs", "connected-device")  # Publish message to “digitest /test1” topic
 mqttc.loop_stop()  # Kill networking daemon
