710269ef92c7558181184c6b2d760ee1ec19317f
Uzair Ahmed <u1ahmed@ryerson.ca>
2023-02-15 14:33:45 -0500

    push 2

--- communication/Testing/top_parsing.py
+++ communication/Testing/top_parsing.py
@@ -2,7 +2,7 @@ import subprocess, re, json
 
 def list_system_stats():
     cmd = 'iw'
-    temp = subprocess.Popen([top], stdout = subprocess.PIPE)
+    temp = subprocess.Popen(['top'], stdout = subprocess.PIPE)
     output = temp.communicate()[0]
     return output.decode('ascii')
 
