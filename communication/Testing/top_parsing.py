a7ffe885772775da669dde70ec7320933c36fd5e
Uzair (Central Pi) <uzairmahmed@gmail.com>
2023-03-09 13:23:42 -0500

    top parsing

--- communication/Testing/top_parsing.py
+++ communication/Testing/top_parsing.py
@@ -2,8 +2,17 @@ import subprocess, re, json
 
 def list_system_stats():
     cmd = 'iw'
-    temp = subprocess.Popen(['top'], stdout = subprocess.PIPE)
+    temp = subprocess.Popen(['top', '-b', '-n', '1'], stdout = subprocess.PIPE)
     output = temp.communicate()[0]
     return output.decode('ascii')
 
-print(list_system_stats())
+
+def parse_system_stats(stats):
+    list_of_stats = stats.split('\n')
+    print(list_of_stats[0])
+    print(list_of_stats[1])
+    print(list_of_stats[2])
+    print(list_of_stats[3])
+    print(list_of_stats[4])
+
+parse_system_stats(list_system_stats())
