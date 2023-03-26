17e763dc6035801e40520369a008f8a52e398f08
Uzair Ahmed <uzairmahmed@gmail.com>
2023-03-26 19:09:14 -0400

    attempt 3

--- webapp/capstone-mj02/src/pages/Controls.js
+++ webapp/capstone-mj02/src/pages/Controls.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import {db} from "../components/firebaseConfig/firebase";
 import {useState, useEffect} from "react";
-import { getDatabase, ref, onValue, set, update, put} from "firebase/database";
+import { getDatabase, ref, onValue, set, update} from "firebase/database";
 import { Slider } from "../components/Slider.jsx";
 
 function Controlspage (){
@@ -34,7 +34,7 @@ function Controlspage (){
     }*/
 
     const PushPower = () => {
-        put(ref(db, iot_device + '/control'), {
+        set(ref(db, iot_device + '/control'), {
             on_off : on_off
           });
     }
