7377e277d519fa6ea89dec4f26d2e3cc78d72af5
Uzair Ahmed <uzairmahmed@gmail.com>
2023-03-26 18:07:03 -0400

    better network logging

--- communication/network_mqtt.py
+++ communication/network_mqtt.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import subprocess, re, json
+import subprocess, re, json, threading, psutil
 
 def list_connected_clients():
     cmd = 'iw'
@@ -7,18 +7,46 @@ def list_connected_clients():
     output = temp.communicate()[0]
     return output.decode('ascii')
 
-def parse_connected_clients(listed):
+def get_hosts():
+    temp = subprocess.Popen(['ubus', 'call', 'luci-rpc', 'getHostHints'], stdout = subprocess.PIPE)
+    output = temp.communicate()[0]
+    raw = json.loads(output.decode('ascii'))
+    temp = {}
+    for key in raw:
+      try: 
+        ip_addr = raw[key]['ipaddrs'][0]
+      except IndexError as e: 
+         ip_addr = "None"
+
+      try: 
+        ip6_addr = raw[key]['ip6addrs'][0]
+      except IndexError as e:
+        ip6_addr = "None"
+
+      try: 
+        hostname = raw[key]['name']      
+      except KeyError as e:
+        hostname = "Unknown"
+
+      temp[key] = {
+        "ip_address" : ip_addr,
+        "ip6_address" : ip6_addr,
+        "hostname_val" :hostname
+      }
+
+    return temp
+
+def parse_connected_clients(listed, addresses):
     jsonList = {}
 
     macSearch = "([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}"
     split_stations = listed.split('Station ')
     del split_stations[0]
     for station in split_stations:
-        temp_mac_address = re.search(macSearch, station).group()
-        jsonList[temp_mac_address] = {}
-
+        temp_mac_address = re.search(macSearch, station).group().upper()
+        jsonList[temp_mac_address] = addresses[temp_mac_address]
+        
         split_lines = station.split('\n')
-        print('new station')
         del split_lines[-1]
         del split_lines[0]
         for line in split_lines:
@@ -36,10 +64,6 @@ def getOSValues():
     total_CPUs = psutil.cpu_count(),
     avg_loads = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
 
-    collector_command = ['service', 'firebase_collector', 'status']
-    server_command = ['service', 'react_server', 'status']
-    kiosk_command = ['service', 'kiosk', 'status']
-
     vals = {
         "total_memory": psutil.virtual_memory().total,
         "used_memory": psutil.virtual_memory().used,
@@ -53,20 +77,23 @@ def getOSValues():
 
     return vals
 
-def send_values_over_mqtt(connected_devices):
+def send_values_over_mqtt(vals):
     mqttc = mqtt.Client("router_node")
     mqttc.connect("192.168.1.143", 1883)  # Connect to (broker, port, keepalive-time)
     
-    mqttc.publish("router/logs", json.dumps(connected_devices))
+    mqttc.publish("router/logs", json.dumps(vals))
 
 def main():  # Create instance of client with client ID “digi_mqtt_test”
     listed = list_connected_clients()
     connected_devices = parse_connected_clients(listed)
     osValues = getOSValues()
-    logDict = {**osValues,connected_devices}
+    logDict = {
+      **osValues,
+      "devices": connected_devices
+      }
     print(logDict)
 
-    send_values_over_mqtt(connected_devices)
+    send_values_over_mqtt(logDict)
 
 if __name__ == '__main__':
     main()
