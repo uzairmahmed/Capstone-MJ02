1712960e5ab5dc4cb93c771ec02b7907074cd34c
Ajay Dhawan <58970472+ajay-00@users.noreply.github.com>
2023-04-06 12:37:11 -0400

    update int

--- hardware/Testing/powerSensor/integration/integration.ino
+++ hardware/Testing/powerSensor/integration/integration.ino
@@ -9,20 +9,28 @@ Adafruit_INA219 ina219;
 String redString = "255";
 String greenString = "255";
 String blueString = "255";
+int count = 0; 
 
-const int redPin = 13;     // 13 corresponds to GPIO13
-const int greenPin = 12;   // 12 corresponds to GPIO12
+
 const int bluePin = 14;    // 14 corresponds to GPIO14
+const int greenPin = 12;   // 12 corresponds to GPIO12
+const int redPin = 13;     // 13 corresponds to GPIO13
+const int relay = 15;
+
 
 
 const int resolution = 256;
 // WiFi
 // const char *ssid = "NW Coextro WIFI"; // Enter your WiFi name
 // const char *password = "wycik96now";  // Enter WiFi password
-const char *ssid = "iPhone"; // Enter your WiFi name
-const char *password = "12345678";  // Enter WiFi password
+// const char *ssid = "iPhone"; // Enter your WiFi name
+// const char *password = "12345678";  // Enter WiFi password
+const char *ssid = "Capstone"; // Enter your WiFi name
+const char *password = "capstoneMJ02";  // Enter WiFi password
 // MQTT Broker
-const char *mqtt_broker = "172.20.10.4"; // Enter your WiFi or Ethernet IP
+
+//const char *mqtt_broker = "172.20.10.4"; // Enter your WiFi or Ethernet IP
+const char *mqtt_broker = "192.168.1.143"; // Enter your WiFi or Ethernet IP
 const char *topic = "iOT_1/logs";
 const char *topic2 = "iOT_1/control";
 const int mqtt_port = 1883;
@@ -57,6 +65,12 @@ void setup() {
   // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
   //ina219.setCalibration_16V_400mA();
  ///////////////////////////////////
+
+ //////////////////////////////
+ //Relay setup
+ pinMode(relay, OUTPUT);
+ digitalWrite(relay, LOW);
+ //////////////////////////////
 
 
 
@@ -94,14 +108,29 @@ void setup() {
  client.subscribe(topic2);
 }
 void callback(char *topic, byte *payload, unsigned int length) {
- Serial.print("Message arrived in topic: ");
- Serial.println(topic);
- Serial.print("Message:");
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  String myString = "";// String((char *) payload);
+  Serial.print(myString);
+  for (int i = 0; i < length; i++) {
+    //This is where the command is recieved 
+    Serial.print((char) payload[i]);
+    myString += ((char) payload[i]);
+  }
+  //Serial.print(myString);
+  String power = "PowerOff";
+  if(power.equals(myString)){
+    Serial.print("\nPowering off");
+    digitalWrite(relay, HIGH);
+  }
+  delay(5000);
+  Serial.print("\nPowering on");
+  digitalWrite(relay, LOW);
+
+
+
  
- for (int i = 0; i < length; i++) {
-   //This is where the command is recieved 
-  Serial.print((char) payload[i]);
- }
  
  Serial.println();
  Serial.println(" - - - - - - - - - - - -");
@@ -109,6 +138,28 @@ void callback(char *topic, byte *payload, unsigned int length) {
 void loop() {
   //This is where the loop is 
   //Add sesnor reading here
+
+  
+  if (count == 0){
+      redString = "255";
+      greenString = "0";
+      blueString = "0";
+      count = count + 1; 
+  }
+  else if(count == 1){
+      redString = "0";
+      greenString = "255";
+      blueString = "0";
+      count = count + 1; 
+  }
+  else{
+      redString = "0";
+      greenString = "0";
+      blueString = "255";
+      count = 0; 
+  }
+ 
+
   analogWrite(redPin, redString.toInt());
   analogWrite(greenPin, greenString.toInt());
   analogWrite(bluePin, blueString.toInt());
@@ -126,6 +177,7 @@ void loop() {
   power_mW = ina219.getPower_mW();
   loadvoltage = busvoltage + (shuntvoltage / 1000);
   
+
   // Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
   // Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
   // Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
@@ -138,9 +190,13 @@ void loop() {
   // string resultant;
   // resultant=to_string(power_mW);
   
-  dtostrf(power_mW, 5, 2, destination);
+  // dtostrf(power_mW, 5, 2, destination);
 
  //Replace the string here for sending the data
- client.publish(topic, destination);
- delay(5000);
+ char buffer[50];
+
+ sprintf(buffer, "{\"current_mA\": %.2f, \"loadvoltage\": %.2f, \"power_mW\": %.2f}", current_mA, loadvoltage, power_mW);
+ Serial.println(buffer);
+ client.publish(topic, buffer);
+ delay(10000);
 }
